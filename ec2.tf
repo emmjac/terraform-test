f10f4c275e4aa455b8a65aa7da9728c7af5de8c2
Emmanuella Ozieh-Jackson <emmaoz@hotmail.com>
2024-12-06 22:20:05 -0500

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,25 @@
-resource "aws_instance" "webserver" {
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.secgroup.id]
-    tags = {
-      "Name" = "Webserver"
+resource "aws_instance" "web2" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
-  
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::992382757497:role/admin"
+  }
+
 }
