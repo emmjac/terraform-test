a3fcded837854b3b22037229b58ef0cf4fbc4bed
Emmanuella Ozieh-Jackson <emmaoz@hotmail.com>
2024-12-06 22:46:41 -0500

    adding ec2

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "aws_s3_bucket" {
-    bucket = "XXXXXXXX"
-    acl = "private"
-    versioning {
-      enabled = true
-    }
-    tags = {
-      Name = "mybucket"
-    }
+# resource "aws_s3_bucket" "aws_s3_bucket" {
+#     bucket = "XXXXXXXX"
+#     acl = "private"
+#     versioning {
+#       enabled = true
+#     }
+#     tags = {
+#       Name = "mybucket"
+#     }
   
-}
+# }
