9d215caee8c6e77891ddcefd153365af3de00daf
emmjac <138400961+emmjac@users.noreply.github.com>
2024-12-06 21:42:56 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,17 @@
-resource "aws_instance" "webserver" {
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.secgroup.id]
-    tags = {
-      "Name" = "Webserver"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
-  
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = ""
+  }
+
 }
